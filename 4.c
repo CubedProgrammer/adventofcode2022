3f61a6ff537fbb5b9889e4767184cff8efda5b8d
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2022-12-25 21:31:29 +0000

    day 4

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void scan(unsigned *restrict s, unsigned *restrict t, unsigned *restrict u, unsigned *restrict v)
+{
+    scanf("%u-%u,%u-%u", s, t, u, v);
+}
+int main(int argl, char *argv[])
+{
+    unsigned fl, fr, sl, sr;
+    unsigned cnt = 0;
+    for(scan(&fl, &fr, &sl, &sr); !feof(stdin); scan(&fl, &fr, &sl, &sr))
+        cnt += (fl >= sl && fr <= sr) || (sl >= fl && sr <= fr);
+    printf("%u\n", cnt);
+    return 0;
+}
